ad3a4d325d56f424b30615e5f3ef216e7e84cd3f
TieuLong21Prosper <128500598+TieuLong21Prosper@users.noreply.github.com>
2023-12-01 22:18:54 +0700

    update flask app file

--- flask_app.py
+++ flask_app.py
@@ -2,9 +2,8 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# A simple list to store tasks
 tasks = [
-    {'id': 1, 'title': 'Learn Python', 'done': False},
+    {'id': 1, 'title': 'Modified Learn Jenkins 'done': False},
     {'id': 2, 'title': 'Build a Flask App', 'done': False}
 ]
 
@@ -13,7 +12,7 @@ tasks = [
 def get_tasks():
     return jsonify({'tasks': tasks})
 
-# Define a route for the endpoint '/tasks/<int:task_id>'
+# Define a route for the endpoint '/tasks', method GET
 @app.route('/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
     task = next((task for task in tasks if task['id'] == task_id), None)
@@ -35,6 +34,5 @@ def create_task():
     return jsonify({'task': new_task}), 201
 
 if __name__ == '__main__':
-    # Use environment variables to set host and port dynamically
     app.run(debug=True, host='0.0.0.0', port=5000)
 
